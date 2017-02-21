8f42d4479033df28a5b0e83d8a18da77d00e627a
Markus Fleck <markus.fleck@campus.tu-berlin.de>
2017-02-21 12:42:19 +0100

    Add minimal example (Bouncing Ball)

---
+++ ResearchDemonstrator/Assets/ResearchDemonstrator/Scripts/BounceBehavior.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace IAT.ResearchDemonstrator
+{
+
+    public class BounceBehavior : MonoBehaviour {
+
+        public float forceEnergy;
+
+        private Rigidbody rb;
+
+	    void Start () {
+            rb = GetComponent<Rigidbody>();
+	    }
+	
+        private void OnCollisionEnter(Collision collision)
+        {
+            rb.AddForce(Vector3.up * forceEnergy);
+        }
+    }
+
+}
