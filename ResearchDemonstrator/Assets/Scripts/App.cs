3460a1fd86c45d170a3a9210779c3f60b281f225
Markus Fleck <markus.fleck@campus.tu-berlin.de>
2017-03-02 20:01:34 +0100

    Add CommandLine example

---
+++ ResearchDemonstrator/Assets/Scripts/App.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class App : MonoBehaviour {
+
+    private void OnGUI()
+    { 
+        if(AppStartUp.options == null)
+        {
+            GUILayout.Label("RuntimeLoad not done");
+            return;
+        }
+
+
+        GUILayout.Label(AppStartUp.options.aMessage);
+    }
+}
---
+++ ResearchDemonstrator/Assets/Scripts/AppStartUp.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using CommandLine;
+
+public static class AppStartUp {
+
+    public static Options options;
+
+    [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
+    static void GetCommandLineArguments()
+    {
+        var args = Environment.GetCommandLineArgs();
+
+        options = new Options();
+
+        Parser.Default.ParseArguments(args, options);
+    }
+
+}
+
+public class Options
+{
+    [Option('m', "message", DefaultValue = "noMessage", HelpText = "A Message displayed by the app")]
+    public string aMessage { get; set; }
+}
